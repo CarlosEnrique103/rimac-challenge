0c1dd345e01ebac1b0f8b311d0475593f5338f72
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2025-10-25 20:25:00 -0500

    fix: try to solve error turbopack

--- next.config.ts
+++ next.config.ts
@@ -13,45 +13,19 @@
 
 // export default nextConfig;
 
-// import type { NextConfig } from "next";
-
-// const nextConfig = {
-//   experimental: {
-//     svgr: true,
-//   },
-// } satisfies NextConfig as any; // 👈 ignora el error de tipo
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    },
-  },
-
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.(".svg")
-    );
-
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|jsx|tsx)$/] },
+      test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
+  // experimental: {
+  //   turbo: false,
+  // },
 };
 
 export default nextConfig;
